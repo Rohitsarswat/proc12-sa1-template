3cef22f85945d339792df3c3caed86d266b336ea
Rohitsarswat <83912817+Rohitsarswat@users.noreply.github.com>
2021-10-09 17:33:48 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,18 @@
 var marks = [35,38,42,45,43,34,46,41,48,32];
 
+function squares(Arrayname)
+{
+  for (var i = 0; i < Arrayname.lenght; i++ ){
+    Arrayname[i] = Araayname[i]*Arrayname[i]
+  }
+ return Arrayname 
+}
 
 function setup() 
 {
-  createCanvas(400,400);
+ var results = squares(marks)
+ console.log(results)
 
-}
-
-function draw() 
-{
-  background(30);
 }
 
  
